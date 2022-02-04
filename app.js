a7e15474bf1262a493ca6bfbb16a01ec6f3ca982
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-04 22:48:17 +0000

    we can send username now

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/currenttime', (req, res)=>{
+app.get('/currenttime', (req, res) => {
   res.send('<h1>' + new Date().toISOString() + '</h1>')
 })//localhost:3311/currenttime
-app.get('/', (req, res)=>{
-  res.send('<h1>Hello Max</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello User</h1><form action="/store-user" method="POST"><label for="name">Your Name</label><br><input type="text" name="username" id="name"><br><button type="submit">Submit</button></form>')
 })//localhost:3311/
+
+app.post('/store-user', function (req, res) {
+  const username = req.body.username
+  console.log(username)
+  res.send('<h1>Username stored!</h1>')
+})
 
 app.listen(3311)
